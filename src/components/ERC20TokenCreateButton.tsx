8250385459b282fc6173e1620119a3bfd3bc06c2
Elmer <s0227373691@gms.tku.edu.tw>
2024-01-29 04:58:54 +0800

    update style

---
+++ src/components/ERC20TokenCreateButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ERC20TokenCreateButton = () => {
+    return (
+        <button
+            className="group inline-block rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px] text-red-500 hover:text-white focus:outline-none focus:ring active:text-opacity-75"
+        >
+            <span
+                className="block rounded-full bg-white px-8 py-3 text-sm font-medium group-hover:bg-transparent"
+            >
+                Create
+            </span>
+        </button>
+    )
+}
+
+export default ERC20TokenCreateButton
